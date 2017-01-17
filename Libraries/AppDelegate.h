018d5dca7e23f9c9b6b742d0d16ba151588d1826
NicholasTEllis <e.nicholas72@yahoo.com>
2017-01-17 13:30:17 -0700

    Initial Commit

---
+++ Libraries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Libraries
+//
+//  Created by Nicholas Ellis on 1/17/17.
+//  Copyright © 2017 Nicholas Ellis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Libraries/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Libraries
+//
+//  Created by Nicholas Ellis on 1/17/17.
+//  Copyright © 2017 Nicholas Ellis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
